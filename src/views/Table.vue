c0f9bb71b2f406319251e73c1bd9d0ecd4a5a4cb
MingXu-CHN <251041263@qq.com>
2022-07-01 15:06:56 +0800

    table自定义列模板

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="tableData" style="width: 100%"
+    stripe  border>
     <el-table-column prop="name" label="姓名"> </el-table-column>
     <el-table-column label="年龄">
       <template slot-scope="scope"> {{ scope.row.age }} 周岁 </template>
@@ -11,13 +12,18 @@
       </template>
     </el-table-column>
     <el-table-column label="操作" align="center">
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+      <template slot-scope="scope">
+        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        <el-button 
+          @click="del(scope.$index)"
+          type="danger" icon="el-icon-delete" circle></el-button>
+      </template>
+
     </el-table-column>
   </el-table>
 </template>
 
-  <script>
+<script>
 export default {
   data() {
     return {
@@ -29,5 +35,10 @@ export default {
       ],
     };
   },
+  methods: {
+    del(index){
+      alert(index)
+    }
+  }
 };
 </script>
