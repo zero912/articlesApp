f46125518e379085356f28d355e36b96591d3ae6
MingXu-CHN <251041263@qq.com>
2022-07-04 14:41:28 +0800

    表单验证（3）

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -14,11 +14,20 @@
       v-model="name"
       :state="nameState"
       @blur.native.capture="checkName"></mt-field>
-    <mt-field type="password" placeholder="请输入密码" label="密码"></mt-field>
+    <mt-field 
+      type="password" 
+      placeholder="请输入密码" 
+      label="密码"
+      v-model="pwd"
+      :state="pwdState"
+      @blur.native.capture="checkPwd"></mt-field>
     <mt-field
       type="password"
       placeholder="请再次输入密码"
       label="确认密码"
+      v-model="repwd"
+      :state="repwdState"
+      @blur.native.capture="checkRepwd"
     ></mt-field>
     <mt-button size="large" type="primary">立即注册</mt-button>
   </div>
@@ -29,6 +38,10 @@ export default {
     return {
       name: '',  // 绑定用户名
       nameState: '', // 用户名输入框的状态
+      pwd: '',
+      pwdState: '',
+      repwd: '',
+      repwdState: '',
     }
   },
   methods: {
@@ -41,10 +54,20 @@ export default {
       }
     },
     checkPwd(){  /** 验证密码 6位数字 */
-
+      let exp = /^\d{6}$/;
+      if(exp.test(this.pwd)){  // 验证通过
+        this.pwdState = 'success'
+      }else {  // 验证失败
+        this.pwdState = 'error'
+      }
     },
     checkRepwd(){  /** 验证重复密码 与密码相同 */
-
+      let exp = /^\d{6}$/;
+      if(exp.test(this.repwd) && this.pwd===this.repwd){  // 验证通过
+        this.repwdState = 'success'
+      }else {  // 验证失败
+        this.repwdState = 'error'
+      }
     },
   }
 }
