20539b459a7b4e5399b8b57a83db8c416780c406
MingXu-CHN <251041263@qq.com>
2022-07-04 11:29:14 +0800

    实现注册页面

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,5 +6,14 @@
       <mt-button icon="back" slot="left"></mt-button>
       <router-link to="#" slot="right">登录</router-link>
     </mt-header>
+    <!-- 表单 -->
+    <mt-field type="text" placeholder="请输入用户名" label="用户名"></mt-field>
+    <mt-field type="password" placeholder="请输入密码" label="密码"></mt-field>
+    <mt-field
+      type="password"
+      placeholder="请再次输入密码"
+      label="确认密码"
+    ></mt-field>
+    <mt-button size="large" type="primary">立即注册</mt-button>
   </div>
 </template>
