5f57d1847ebfc3ad3e15abcbce36bdfce9e3289a
MingXu-CHN <251041263@qq.com>
2022-07-04 12:01:30 +0800

    表单验证（1）

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,7 +7,12 @@
       <router-link to="#" slot="right">登录</router-link>
     </mt-header>
     <!-- 表单 -->
-    <mt-field type="text" placeholder="请输入用户名" label="用户名"></mt-field>
+    <mt-field 
+      type="text" 
+      placeholder="请输入用户名" 
+      label="用户名"
+      v-model="name"
+      @blur.native="checkName"></mt-field>
     <mt-field type="password" placeholder="请输入密码" label="密码"></mt-field>
     <mt-field
       type="password"
@@ -17,3 +22,25 @@
     <mt-button size="large" type="primary">立即注册</mt-button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      name: '',  // 绑定用户名
+    }
+  },
+  methods: {
+    checkName(){  /** 验证用户名 */
+      let exp = /^\w{3,15}$/;
+      if(exp.test(this.name)){  // 验证通过
+        console.log('通过..');
+      }else {  // 验证失败
+        console.log('失败..');
+      }
+    }
+  }
+}
+</script>
+
+
+
